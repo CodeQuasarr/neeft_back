d91e863b2a30fee4f0fd4c5597087351f4b84f01
Samuel ANYARONKE <75504658+Sanyaronke@users.noreply.github.com>
2022-12-06 13:27:29 +0100

    ticket_#4

--- app/models/users/user.go
+++ app/models/users/user.go
@@ -9,10 +9,10 @@ import "time"
 type User struct {
 	ID              uint   `gorm:"primaryKey"   json:"id" `
 	Username        string `gorm:"varchar(255)" json:"username"`
-	FirstName       string `gorm:"varchar(255)" json:"first_name"`
-	LastName        string `gorm:"varchar(255)" json:"last_name"`
+	FirstName       string `gorm:"varchar(255)" json:"firstName"`
+	LastName        string `gorm:"varchar(255)" json:"lastName"`
 	Email           string `gorm:"varchar(255)" json:"email"`
-	EmailVerifiedAt bool   `gorm:"boolean"      json:"email_verified_at"`
+	EmailVerifiedAt bool   `gorm:"boolean"      json:"emailVerifiedAt"`
 	Password        string `gorm:"varchar(255)" json:"password"`
 	Created_at      time.Time
 	Updated_at      time.Time
